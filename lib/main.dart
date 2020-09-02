ad0e6f249a74125da093959d6ec7601fb14b9200
Sean Xiao <jean.paul.1685@outlook.com>
2020-09-01 21:26:11 -0400

    Finish google course

--- lib/main.dart
+++ lib/main.dart
@@ -6,23 +6,102 @@ void main() => runApp(HyperPro());
 class HyperPro extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-    final wordPair = WordPair.random();
-
     return MaterialApp(
-      title: 'HyperPro',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        title: 'HyperPro',
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: RandomWords(),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = TextStyle(fontSize: 18.0);
+  final _saved = Set<WordPair>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HyperPro'),
+        actions: [
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('HyperPro'),
-        ),
-        body: Center(
-          child: Text(wordPair.asPascalCase),
-        ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final tiles = _saved.map((WordPair pair) {
+            return ListTile(
+              title: Text(
+                pair.asCamelCase,
+                style: _biggerFont,
+              ),
+            );
+          });
+
+          final divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text("Favorite Hypros"),
+            ),
+            body: ListView(children: divided),
+          );
+        }
       )
     );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+    return ListTile(
+      title: Text(
+        pair.asCamelCase,
+        style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return Divider();
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
   }
 }
